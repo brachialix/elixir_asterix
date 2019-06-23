91c98d0c2ba4b2ba7ee6b0d0ad3ed7c5b8661481
Alexander Wemmer <alex@wemmer.at>
2019-06-23 19:23:46 +0200

    added exdoc

--- mix.exs
+++ mix.exs
@@ -8,7 +8,9 @@ defmodule Asterix.MixProject do
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      escript: escript()
+      escript: escript(),
+      name: "Elixir ASTERIX",
+      source_url: "https://github.com/brachialix/elixir_asterix"
     ]
   end
 
@@ -22,6 +24,7 @@ defmodule Asterix.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
