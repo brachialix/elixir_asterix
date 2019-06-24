8d6957ac5c0e1f60f584b5292b9c356220d5bf7b
Alexander Wemmer <alex@wemmer.at>
2019-06-24 20:21:01 +0200

    tried differend TDD format

--- test/asterix_decode_fields_test.exs
+++ test/asterix_decode_fields_test.exs
@@ -7,18 +7,17 @@ defmodule Asterix.Decode.FieldsTest do
 
   describe "decoding: unsigned_number_field : 1 octet, factor 1" do
     setup do
-      {:ok,
-        test_data: [
-          {[<<255>>],                 {%{testfield: 255},   []}},
-          {[<<0>>, <<0>>],            {%{testfield: 0},     [<<0>>]}},
-          {[<<16>>, <<100>>],         {%{testfield: 16},    [<<100>>]}},
-          {[<<31>>, <<127>>, <<10>>], {%{testfield: 31},    [<<127>>, <<10>>]}}
-        ]}
+      [ testdata: [
+          %{ in:  [<<255>>],                  out: {%{testfield: 255}, []}},
+          %{ in:  [<<0>>, <<0>>],             out: {%{testfield: 0}, [<<0>>]}},
+          %{ in:  [<<16>>, <<100>>],          out: {%{testfield: 16}, [<<100>>]}},
+          %{ in:  [<<31>>, <<127>>, <<10>>],  out: {%{testfield: 31}, [<<127>>, <<10>>]}},
+      ]]
     end
 
-    test "", %{test_data: test_data} do
-      Enum.each(test_data, fn {data, expected_value} ->
-        assert Fields.unsigned_number_field(data, 1, :testfield, 1)  == expected_value
+    test "", fixtures do
+      Enum.each(fixtures.testdata, fn testdata ->
+        assert Fields.unsigned_number_field(testdata.in, 1, :testfield, 1)  == testdata.out
       end)
     end
   end
@@ -26,7 +25,7 @@ defmodule Asterix.Decode.FieldsTest do
   describe "decoding: unsigned_number_field : 1 octet, factor 3" do
     setup do
       {:ok,
-        test_data: [
+        testdata: [
           {[<<255>>],                 {%{testfield: 765},   []}},
           {[<<0>>, <<0>>],            {%{testfield: 0},     [<<0>>]}},
           {[<<16>>, <<100>>],         {%{testfield: 48},    [<<100>>]}},
@@ -34,8 +33,8 @@ defmodule Asterix.Decode.FieldsTest do
         ]}
     end
 
-    test "", %{test_data: test_data} do
-      Enum.each(test_data, fn {data, expected_value} ->
+    test "", %{testdata: testdata} do
+      Enum.each(testdata, fn {data, expected_value} ->
         assert Fields.unsigned_number_field(data, 1, :testfield, 3)  == expected_value
       end)
     end
@@ -44,7 +43,7 @@ defmodule Asterix.Decode.FieldsTest do
   describe "decoding: unsigned_number_field : 2 octets, factor 1" do
     setup do
       {:ok,
-        test_data: [
+        testdata: [
           {[<<0>>, <<0>>],            {%{testfield: 0},     []}},
           {[<<1>>, <<1>>],            {%{testfield: 257},   []}},
           {[<<0>>, <<100>>],          {%{testfield: 100},   []}},
@@ -52,8 +51,8 @@ defmodule Asterix.Decode.FieldsTest do
         ]}
     end
 
-    test "", %{test_data: test_data} do
-      Enum.each(test_data, fn {data, expected_value} ->
+    test "", %{testdata: testdata} do
+      Enum.each(testdata, fn {data, expected_value} ->
         assert Fields.unsigned_number_field(data, 2, :testfield, 1)  == expected_value
       end)
     end
@@ -65,7 +64,7 @@ defmodule Asterix.Decode.FieldsTest do
   describe "decoding: signed_number_field : 1 octet, factor 1" do
     setup do
       {:ok,
-        test_data: [
+        testdata: [
           {[<<128>>],                 {%{testfield: -128},  []}},
           {[<<0>>, <<0>>],            {%{testfield: 0},     [<<0>>]}},
           {[<<255>>, <<255>>],        {%{testfield: -1},    [<<255>>]}},
@@ -73,8 +72,8 @@ defmodule Asterix.Decode.FieldsTest do
         ]}
     end
 
-    test "", %{test_data: test_data} do
-      Enum.each(test_data, fn {data, expected_value} ->
+    test "", %{testdata: testdata} do
+      Enum.each(testdata, fn {data, expected_value} ->
         assert Fields.signed_number_field(data, 1, :testfield, 1)  == expected_value
       end)
     end
@@ -83,7 +82,7 @@ defmodule Asterix.Decode.FieldsTest do
   describe "decoding: signed_number_field : 1 octet, factor 3" do
     setup do
       {:ok,
-        test_data: [
+        testdata: [
           {[<<128>>],                 {%{testfield: -384},  []}},
           {[<<0>>, <<0>>],            {%{testfield: 0},     [<<0>>]}},
           {[<<255>>, <<255>>],        {%{testfield: -3},    [<<255>>]}},
@@ -91,8 +90,8 @@ defmodule Asterix.Decode.FieldsTest do
         ]}
     end
 
-    test "", %{test_data: test_data} do
-      Enum.each(test_data, fn {data, expected_value} ->
+    test "", %{testdata: testdata} do
+      Enum.each(testdata, fn {data, expected_value} ->
         assert Fields.signed_number_field(data, 1, :testfield, 3)  == expected_value
       end)
     end
@@ -101,7 +100,7 @@ defmodule Asterix.Decode.FieldsTest do
   describe "decoding: signed_number_field : 2 octets, factor 1" do
     setup do
       {:ok,
-        test_data: [
+        testdata: [
           {[<<128>>, <<0>>],          {%{testfield: -32768},[]}},
           {[<<1>>, <<1>>],            {%{testfield: 257},   []}},
           {[<<0>>, <<100>>],          {%{testfield: 100},   []}},
@@ -109,8 +108,8 @@ defmodule Asterix.Decode.FieldsTest do
         ]}
     end
 
-    test "", %{test_data: test_data} do
-      Enum.each(test_data, fn {data, expected_value} ->
+    test "", %{testdata: testdata} do
+      Enum.each(testdata, fn {data, expected_value} ->
         assert Fields.signed_number_field(data, 2, :testfield, 1)  == expected_value
       end)
     end
